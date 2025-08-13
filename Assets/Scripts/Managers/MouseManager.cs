98fe1f74982fd26a5febf4d211b9eb473622f9ae
yunqi <2658999841@qq.com>
2025-08-13 22:35:36 +0800

    鼠标点击移动

---
+++ Assets/Scripts/Managers/MouseManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+//using UnityEngine.Events;
+
+//[System.Serializable]
+//public class EventVector3 : UnityEvent<Vector3> { }
+
+public class MouseManager : MonoBehaviour
+{
+    public static MouseManager Instance;
+
+    public Texture2D point, doorway, attack, target, arrow;
+    RaycastHit hitInfo;
+    public event Action<Vector3> OnMouseClicked;
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Destroy(gameObject);
+
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        SetCursorTexture();
+        MouseControl();
+    }
+
+    void SetCursorTexture()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            //«–ªª Û±ÍÃ˘Õº
+            switch (hitInfo.collider.gameObject.tag)
+            {
+                case "Ground":
+                    Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+                    break;
+            }
+        }
+    }
+
+    void MouseControl()
+    {
+        if (Input.GetMouseButtonDown(0) && hitInfo.collider != null)
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Ground"))
+                OnMouseClicked?.Invoke(hitInfo.point);
+        }
+    }
+}
